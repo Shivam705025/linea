5fd1b2708f7681622fdde61427ab65536324027e
Shivam705025 <48374957+Shivam705025@users.noreply.github.com>
2023-07-19 23:36:16 +0530

    Add files via upload

---
+++ js/pages-%5B%5B...slug%5D%5D-569d520de74ff8f7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[120],{5759:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[[...slug]]",function(){return u(3270)}])}},function(n){n.O(0,[802,798,270,774,888,179],function(){return n(n.s=5759)}),_N_E=n.O()}]);
